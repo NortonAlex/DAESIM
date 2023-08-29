7f2b85f8ef70dafdbf5f135aecd2e3bb2f602fa1
Alexander Norton <alexander.norton@climate-energy-college.org>
2023-08-29 22:30:44 +1000

    Update to Python 3.9, added jupytext
    Updated to Python 3.9 to resolve package conflicts. Also included
    jupytext dependency and use so that python notebooks (*.ipynb) are
    automatically duplicated as standard .py files, which makes
    tracking in git much much easier!

---
+++ notebooks/Untitled.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import daesim.example
+
+# %%

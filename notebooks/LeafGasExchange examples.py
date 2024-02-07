cc3916d785689dbd67939b14f79cd87952200cdf
Alexander Norton <alexander.norton@climate-energy-college.org>
2024-02-08 10:48:21 +1100

    Added new photosynthesis-stomatal cond model
    Built a new module to describe leaf gas exchange (CO2, H2O). This
    uses the photosynthesis model of Johnson and Berry (2021) and
    stomatal conductance model of Medlyn et al. (2011). It still needs
    temperature resposne functions to be applied to the biochemical
    parameters.

---
+++ notebooks/LeafGasExchange examples.py
@@ -0,0 +1,163 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %load_ext nb_black
+
+# %%
+import numpy as np
+import pandas as pd
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+# %%
+from daesim.leafgasexchange import LeafGasExchangeModule
+from daesim.climate import *
+
+# %% [markdown]
+# ### Photosynthesis Model
+#
+# We use the mechanistic C3 photosynthesis model described by Johnson and Berry (2021, doi: 10.1007/s11120-021-00840-4). This model extends the Farquhar et al. (1980, 149:78–90) model of C3 photosynthesis by incorporating a mechanistic description of steady-state electron transport rate. In the new model, the dark reactions of photosynthesis (carbon metabolism) are described by the traditional Farquhar et al. (1980, 149:78–90) model while light reactions of photosynthesis (electron transport) are described by the new Johnson and Berry (2021, doi: 10.1007/s11120-021-00840-4) model. This new model also derives fluorescence parameters from the mechanistic gas-exchange and electron transport expressions which can be compared to pulse-amplitude-modulated fluorescence measurements. 
+#
+# The potential rate of net CO2 assimilation under electron transport limitation (light-limited), $A_j$, is given by:
+#
+# $A_j = \frac{J_{P680}'}{4+8 \Gamma*/C} (1 - \Gamma*/C) - R_d$
+#
+# Where $R_d$ is dark respiration (mitochondrial respiration), $C$ is the CO2 partial pressure in the chloroplasts, $\Gamma*$ is the CO2 compensation point in the absence of dark respiration, and $J_{P680}'$ is the rate of electron transport through photosystem II. The model provides a conceptual and quantitative description of $J_{P680}'$. 
+#
+# The potential rate net CO2 assimilation under Rubisco limitation (light-saturated), $A_c$, is given by:
+#
+# $A_c = \frac{V_{cmax} C}{K_c (1 + O/K_o) + C} (1 - \Gamma*/C) - R_d$
+#
+# Where $V_{cmax}$ is the maximum carboxylase activity of Rubisco, $K_c$ and $K_o$ are the Michaelis-Menten constants for CO2 and O2, respectively. 
+#
+# ### Stomatal Conductance Model
+#
+# To represent stomatal conductance we implement the model of Medlyn et al. (2011, see Corrigendum doi:10.1111/j.1365-2486.2012.02790.x). This model unified the optimal and empirical approaches to representing 
+# The model presented a unified approach to modeling stomatal conductance and developed an empirical expression that was consistent with the optimal stomatal behaviour as first described by Cowan and Farquhar (1977). This optimal stomatal behaviour theory postulates that stomata should act to maximize carbon gain (photosynthesis, $A$) while at the same time minimizing water lost ($E$, transpiration).
+#
+# The theoretical model of Equation 11 in Medlyn et al. (2011, see Corrigendum doi:10.1111/j.1365-2486.2012.02790.x) expresses the stomatal conductance of H2O, $g_{sw}$, as:
+#
+# $g_{sw} \approx g_0 + 1.6 (1 + \frac{g_1}{\sqrt{D}}) \frac{A}{C_a}$
+#
+# when D is the leaf-to-air vapor pressure deficit and is expressed in kPa, $C_a$ is the atmospheric CO2 concentration, $A$ is the rate of net CO2 assimilation, $g_0$ and $g_1$ are fitting parameters, where $g_1$ has units of kPa$^{0.5}$ and is proportional to the marginal water cost of carbon, $\lambda$, and with the CO2 compensation point, $\Gamma*$. Importantly, the Medlyn et al. (2011) model uses the light-limited rate of CO2 assimilation for $A$
+#
+# Equation 13 in Medlyn et al. (2011, see Corrigendum doi:10.1111/j.1365-2486.2012.02790.x):
+#
+# $\frac{C_i}{C_a} \approx \frac{g_1}{g_1 + \sqrt{D}}$
+#
+# Equation 14 in Medlyn et al. (2011, see Corrigendum doi:10.1111/j.1365-2486.2012.02790.x), the instantaneous transpiration use efficiency (ITE), is the ratio of leaf photosynthesis to transpiration which can be expressed as:
+#
+# $\frac{A}{E} \approx \frac{C_a P}{1.6(D + g_1 \sqrt{D}}$ 
+#
+# where $P$ is atmospheric pressure in kPa.
+#
+# ### Implementation
+#
+# To implement the combined photosynthesis and stomatal conducance model we take a numerical approach to estimating the optimal $C_i$. In this approach, there are five steps: Step 1 sets an initial estimate of $C_i$. Step 2 calculates A with the estimate of $C_i$. Step 3 updates $g_{sw}$ using the Medlyn et al. (2011) formulation. Step 4 updates $C_i$ using an expression following Fick's law of diffusion which relates the gradient of $C_a$ and $C_i$ to the photosynthetic rate and stomatal conductance. Step 5 evaluates the proximity of the updated $C_i$ to the previous estimate (using an error tolerance limit). If it is outside the tolerance limit, replace $C_i$ in step 1 with the updated estimate and repeat until convergence whereby the optimal $C_i$ is determined. 
+#
+# Once the optimal $C_i$ has been determined, it is passed back into the full photosynthetic rate equations (where both light- and rubisco-limited rates are considered) to determine that actual photosynthetic rate. 
+
+# %% [markdown]
+# ### Initialise the Leaf Gas Exchange model
+
+# %%
+Leaf = LeafGasExchangeModule()#Vcmax_opt=100e-6,Vqmax_opt=350e-6,g1=2.5,TPU_opt_rVcmax=0.25)
+
+# %% [markdown]
+# ### Run the model given input data
+
+# %%
+p = 101325 # air pressure, Pa
+
+Q = 1200e-6  # absorbed PPFD, umol PAR m-2 s-1
+T = 25.0  # leaf emperature, degrees Celcius
+Cs = 400*(p/1e5)*1e-6 # carbon dioxide partial pressure, bar
+O = 209000*(p/1e5)*1e-6 # oxygen partial pressure, bar
+RH = 65.0  # relative humidity, %
+
+A, gs, Ci, Vc, Ve, Vs, Rd = Leaf.calculate(Q,T,Cs,O,RH)
+
+# %% [markdown]
+# ### Simulate a light-response curve
+
+# %%
+n = 50
+
+p = 101325*np.ones(n) # air pressure, Pa
+Q = np.linspace(50,2400,n)*1e-6  #1200*np.ones(n)  # umol PAR m-2 s-1
+T = 25.0*np.ones(n)  # degrees Celcius
+Cs = 400*(p/1e5)*1e-6*np.ones(n) # carbon dioxide partial pressure, bar
+O = 209000*(p/1e5)*1e-6*np.ones(n) # oxygen partial pressure, bar
+RH = 65.0*np.ones(n)
+
+A, gs, Ci, Vc, Ve, Vs, Rd = Leaf.calculate(Q,T,Cs,O,RH)
+
+fig, axes = plt.subplots(1,2,figsize=(10,4))
+axes[0].plot(Q*1e6,A*1e6,label="Anet",c="0.5")
+axes[0].plot(Q*1e6,A*1e6+Rd*1e6,label="Anet+Rd",c="k")
+axes[0].plot(Q*1e6,Vc*1e6,label="Vc",linestyle=":")
+axes[0].plot(Q*1e6,Ve*1e6,label="Ve",linestyle=":")
+axes[0].legend()
+axes[0].set_ylim([-5,70])
+axes[0].set_ylabel(r"$\rm A$"+"\n"+r"($\rm \mu mol \; m^{-2} \; s^{-1}$)");
+axes[0].set_xlabel(r"$\rm Q_{abs}$"+"\n"+r"($\rm \mu mol \; m^{-2} \; s^{-1}$)");
+
+axes[1].plot(Q*1e6,gs)
+axes[1].set_xlabel(r"$\rm Q_{abs}$"+"\n"+r"($\rm \mu mol \; m^{-2} \; s^{-1}$)");
+axes[1].set_ylabel(r"$\rm g_{sw}$"+"\n"+r"($\rm mol \; m^{-2} \; s^{-1}$)");
+
+axes[0].set_title("Light-response curve: Photosynthetic rate")
+axes[1].set_title("Light-response curve: Stomatal conductance")
+
+plt.tight_layout()
+
+# %% [markdown]
+# ### Simulate a CO2-response curve
+
+# %%
+n = 50
+
+p = 101325*np.ones(n) # air pressure, Pa
+Q = 800e-6*np.ones(n)  # umol PAR m-2 s-1
+T = 25.0*np.ones(n)  # degrees Celcius
+Cs = np.linspace(60,800,n)*(p/1e5)*1e-6   #   400*(p/1e5)*1e-6*np.ones(n) # carbon dioxide partial pressure, bar
+O = 209000*(p/1e5)*1e-6*np.ones(n) # oxygen partial pressure, bar
+RH = 65.0*np.ones(n)
+
+A, gs, Ci, Vc, Ve, Vs, Rd = Leaf.calculate(Q,T,Cs,O,RH)
+
+fig, axes = plt.subplots(1,2,figsize=(10,4))
+
+axes[0].plot(Cs*1e6,A*1e6,label="Anet",c="0.5")
+axes[0].plot(Cs*1e6,A*1e6+Rd*1e6,label="Anet+Rd",c="k")
+axes[0].plot(Cs*1e6,Vc*1e6,label="Vc",linestyle=":")
+axes[0].plot(Cs*1e6,Ve*1e6,label="Ve",linestyle=":")
+axes[0].legend()
+axes[0].set_ylim([-5,50])
+axes[0].set_ylabel(r"$\rm A$"+"\n"+r"($\rm \mu mol \; m^{-2} \; s^{-1}$)");
+axes[0].set_xlabel(r"$\rm C_{s}$"+"\n"+r"($\rm \mu mol \; mol^{-1}$)");
+axes[0].grid(True)
+
+axes[1].plot(Cs*1e6,gs)
+axes[1].set_ylabel(r"$\rm g_{sw}$"+"\n"+r"($\rm mol \; m^{-2} \; s^{-1}$)");
+axes[1].set_xlabel(r"$\rm C_{s}$"+"\n"+r"($\rm \mu mol \; mol^{-1}$)");
+axes[1].grid(True)
+
+axes[0].set_title("CO2-response curve: Photosynthetic rate")
+axes[1].set_title("CO2-response curve: Stomatal conductance")
+
+plt.tight_layout()
+
+# %%
---
+++ src/daesim/leafgasexchange.py
@@ -0,0 +1,328 @@
+"""
+Leaf gas exchange model class: Includes equations, calculators, and parameters
+"""
+
+import numpy as np
+from typing import Tuple, Callable
+from attrs import define, field
+from scipy.optimize import OptimizeResult
+from scipy.integrate import solve_ivp
+from daesim.biophysics_funcs import func_TempCoeff, growing_degree_days_DTT_nonlinear, growing_degree_days_DTT_linear1, growing_degree_days_DTT_linear2, growing_degree_days_DTT_linear3
+from daesim.management import ManagementModule
+from daesim.climate import ClimateModule
+from daesim.climate_funcs import solar_day_calcs
+
+@define
+class LeafGasExchangeModule:
+    """
+    Calculator of leaf gas exchange including photosynthesis and stomatal conductance
+    """
+
+    # Class parameters
+
+    ## Biochemical constants
+    Kc_opt: float = field(
+        default=405.0*1e-06
+    )  ## Kinetic coefficient for CO2 (VonCaemmerer and Furbank, 1999), bar
+    Ko_opt: float = field(
+        default=278.0*1e-03
+    )  ## Kinetic coefficient for O2 (VonCaemmerer and Furbank, 1999), bar
+
+    spfy_opt: float = field(
+        default=2600.0
+    )  ## Specificity (tau in Collatz e.a. 1991). This is, in theory, Vcmax/Vomax.*Ko./Kc, but used as a separate parameter.
+
+    Vcmax_opt: float = field(
+        default=100.0*1e-6
+    )  ## Maximum Rubisco activity at optimum temperature, mol CO2 m-2 s-1
+    Vqmax_opt: float = field(
+        default=350.0*1e-6
+        ) ## Maximum Cyt b6f activity at optimum temperature, mol e-1 m-2 s-1
+    TPU_opt_rVcmax: float = field(default=0.1666)  ## TPU as a ratio of Vcmax_opt (from Bonan, 2019, Chapter 11, p. 171)
+    Abs: float = field(default=0.85)  ## Total leaf absorptance to PAR, mol PPFD absorbed mol-1 PPFD incident
+    beta: float = field(default=0.52)  ## PSII fraction of total leaf absorptance, mol PPFD absorbed by PSII mol-1 PPFD absorbed
+    Rds: float = field(default=0.01)  ## Scalar for dark respiration, dimensionless
+
+    nl: float = field(default=0.75) ## ATP per e- in linear flow, ATP/e-
+    nc: float = field(default=1.00) ## ATP per e- in cyclic flow, ATP/e-
+    effcon: float = field(default=0.25)  ## Efficiency of conversion. TODO: Add better notes here
+    atheta: float = field(default=1)  ## Empirical smoothing parameter to allow for co-limitation of Vc and Ve. In Johnson and Berry (2021) model this must equal 1 (i.e. no smoothing). 
+
+    ## Stomatal conductance constants
+    g0: float = field(default=0.0)   ## g0, see Medlyn et al. (2011, doi: 10.1111/j.1365-2486.2012.02790.x) 
+    g1: float = field(default=3.0)   ## g1, see Medlyn et al. (2011, doi: 10.1111/j.1365-2486.2012.02790.x) 
+
+    alpha_opt: str = field(default='static') ## Model choice of static or dynamic absorption cross-section calculations
+
+    ## Temperature dependence functions
+    tempcor_type: str = field(default='PeakedArrhenius')  ## Temperature scaling function choice
+
+    ## Photochemical constants
+    Kf: float = field(default=0.05e09) ## Rate constant for fluoresence at PSII and PSI, s-1
+    Kd: float = field(default=0.55e09) ## Rate constant for constitutive heat loss at PSII and PSI, s-1
+    Kp1: float = field(default=14.5e09) ## Rate constant for photochemistry at PSI, s-1
+    Kn1: float = field(default=14.5e09) ## Rate constant for regulated heat loss at PSI, s-1
+    Kp2: float = field(default=4.5e09) ## Rate constant for photochemistry at PSII, s-1
+    Ku2: float = field(default=0e09) ## Rate constant for exciton sharing at PSII, s-1
+
+    ## Transfer functions for fluorescence
+    eps1: float = field(default=0) ## PS I transfer function, mol PSI F to detector mol-1 PSI F emitted
+    eps2: float = field(default=1) ## PS II transfer function, mol PSII F to detector mol-1 PSII F emitted
+
+    ## Constants
+    rhoa: float = field(default=1.2047)  ## Specific mass of air, kg m-3
+    Mair: float = field(default=28.96)  ## Molecular mass of dry air, g mol-1
+
+    def calculate(
+        self,
+        Q,    ## Absorbed PPFD, mol PAR m-2 s-1
+        T,    ## Leaf temperature, degrees Celcius
+        Cs,   ## Leaf surface CO2 partial pressure, bar, (corrected for boundary layer effects)
+        O,    ## Leaf surface O2 partial pressure, bar, (corrected for boundary layer effects)
+        RH,   ## Relative humidity, %
+        Site=ClimateModule(),   ## It is optional to define Site for this method. If no argument is passed in here, then default setting for Site is the default ClimateModule(). Note that this may be important as it defines many site-specific variables used in the calculations.
+    ) -> Tuple[float]:
+
+        # Calculate derived variables from constants
+        # TODO: Add in temperature response functions to scale temperature-dependent parameters
+        Vqmax = self.Vqmax_opt       # Maximum Cyt b6f activity, mol e-1 m-2 s-1
+        Vcmax = self.Vcmax_opt       # Maximum Rubisco activity, mol CO2 m-2 s-1
+        TPU = self.TPU_opt_rVcmax * self.Vcmax_opt
+        Rd = Vcmax*self.Rds
+        S = self.spfy_opt
+        Kc = self.Kc_opt
+        Ko = self.Ko_opt
+        phi1P_max = self.Kp1/(self.Kp1+self.Kd+self.Kf)  # Maximum photochemical yield PS I
+        Gamma_star   = 0.5 / S * O      # compensation point in absence of Rd
+
+        ## Establish PSII and PS I cross-sections, mol PPFD abs PS2/PS1 mol-1 PPFD
+        ## TODO: consider moving this to a separate function
+        if self.alpha_opt == 'static':
+            a2 = self.Abs*self.beta
+            a1 = self.Abs - a2
+        elif self.alpha_opt == 'dynamic':
+            print("dynamic absorption cross-section not implemented yet")
+            return np.nan
+
+        minCi = 0.3  ## Minimum Ci (as fraction of Cs) for BallBerry Ci.
+
+        MM_consts = Kc * (1.0 + O/Ko)
+
+        VPD = Site.compute_VPD(T,RH)*1e-3
+
+        # Compute stomatal conductance and Ci based on optimal stomatal theory (Medlyn et al., 2011)
+        A, gs, Ci = self.solve_Ci(Cs,Q,O,VPD,Vqmax,a1,phi1P_max,S)    ## TODO: Check the units of A, gs, and Ci here. Is it in ppm (umol mol-1?)? or bar? 
+
+        # Update photosynthetic rate with optimal Ci
+        (A, Ag, Vc, Ve, Vs) = self.compute_A(Ci, O, Q, Vcmax, Vqmax, TPU, Gamma_star, Rd, S, Kc, Ko, a1, phi1P_max)
+
+        # Actual electron transport rate
+        Ja = Ag / ((Ci - Gamma_star) / (Ci + 2 * Gamma_star)) / self.effcon
+
+        # Stomatal resistance
+        rcw    = ( self.rhoa / (self.Mair*1.0e3) )/gs
+
+        return (A, gs, Ci, Vc, Ve, Vs, Rd)
+
+    def JB21_A_c(self, C, O, Vcmax, S, Kc, Ko):  #Vcmax=100e-6, S=2600, Kc=405.0*1e-06, Ko=278.0*1e-03):
+        """
+        Calculates the potential rate of (gross) CO2 assimilation under Rubisco limitation. This is synonymous 
+        with the ‘light-saturated’ or 'Rubisco limited' metabolic state where carbon metabolism is 
+        limiting electron transport.
+        
+        Parameters
+        ----------
+        C = CO2 partial pressure in the chloroplasts, bar, (often denoted $C_c$ when mesophyll conductance is considered and $C_i$ when mesophyll conductance is ignored)
+        O = O2 partial pressure in the chloroplasts, bar
+        Vcmax = Maximum carboxylase activity of Rubisco, mol CO2 m-2 s-1
+        S = Rubisco specificity for CO2/O2, dimensionless
+
+        Returns
+        -------
+        A_c = Potential rate of gross CO2 assimilation, mol CO2 m-2 s-1
+
+        References
+        ----------
+        Johnson and Berry (2021) The role of Cytochrome b6f in the control of steady-state photosynthesis: a conceptual and quantitative model, doi: 10.1007/s11120-021-00840-4
+        """
+        
+        Gamma_star = O/(2*S)  # Calculate CO2 compensation point in the light, $\Gamma^*$
+        
+        eta = (1-(self.nl/self.nc)+(3+7*Gamma_star/C)/((4+8*Gamma_star/C)*self.nc))  # Calculate PS I/II ETR
+
+        # Expressions for potential Rubisco-limited rates (_c)
+        # N.B., see Eqns. 32-33
+        Vc_c = C*Vcmax/(Kc*(1+O/Ko + C))
+        Vo_c = Vc_c*2*Gamma_star/C
+        Ag_c = Vc_c - Vo_c/2
+        return Ag_c
+
+    def JB21_A_j(self, Q, C, O, Vqmax, a1, phi1P_max, S):
+        """
+        Calculates the potential rate of CO2 assimilation under Cyt b6f limitation. This is synonymous 
+        with the ‘light-limited’ or 'RuBP-regeneration limited' metabolic state where electron transport is 
+        limiting carbon metabolism.
+        
+        Parameters
+        ----------
+        Q = absorbed photosynthetically active radiation (PPFD), mol PAR m-2 s-1
+        C = CO2 partial pressure in the chloroplasts, bar, (often denoted $C_c$ when mesophyll conductance is considered and $C_i$ when mesophyll conductance is ignored)
+        O = O2 partial pressure in the chloroplasts, bar
+        Vqmax = Maximum Cyt b6f activity, mol e-1 m-2 s-1
+        a1 = PS I cross-section, mol PPFD abs PS2/PS1 mol-1 PPFD
+        phi1P_max = Maximum photochemical yield PS I, dimensionless
+        S = Rubisco specificity for CO2/O2, dimensionless
+
+        Returns
+        -------
+        A_j = Potential rate of gross CO2 assimilation, mol CO2 m-2 s-1
+
+        References
+        ----------
+        Johnson and Berry (2021) The role of Cytochrome b6f in the control of steady-state photosynthesis: a conceptual and quantitative model, doi: 10.1007/s11120-021-00840-4
+        """
+        
+        Gamma_star = O/(2*S)  # Calculate CO2 compensation point in the light, $\Gamma^*$
+        
+        eta = (1-(self.nl/self.nc)+(3+7*Gamma_star/C)/((4+8*Gamma_star/C)*self.nc))  # Calculate PS I/II ETR
+
+        #% Expressions for potential Cytochrome b6f-limited rates
+        JP700_j = (Q*Vqmax)/(Q+Vqmax/(a1*phi1P_max))
+        JP680_j = JP700_j/eta
+        Vc_j = JP680_j/(4*(1+2*Gamma_star/C))
+        Vo_j = Vc_j*2*Gamma_star/C
+        Ag_j = Vc_j - Vo_j/2
+        return Ag_j
+
+    def net_co2_assimilation(self,Ag,Rd):
+        """
+        Ag = gross photosynthetic CO2 assimilation, mol CO2 m-2 s-1
+        Rd = Mitochondrial respiration, mol CO2 m-2 s-1
+        """
+        return Ag - Rd
+        
+    def gs_Medlyn(self,Cs,D,An):
+        """
+        Model for the optimal stomatal conductance to water vapour following Medlyn et al. (2011)
+        
+        Parameters
+        ----------
+        Cs: float
+            Atmospheric carbon dioxide partial pressure at the leaf surface, umol mol-1
+        D: float
+            Leaf-to-air vapour pressure deficit, kPa
+        An: float
+            Net assimilation rate of CO2, umol CO2 m-2 s-1
+        g1, g0: float
+            Fitted parameters according to the Medlyn et al. (2011) model
+
+        Returns
+        -------
+        gs: float
+            Optimal stomatal conductance, mol H2O m-2 s-1
+
+        References
+        ----------
+        See original paper and corrigendum: Medlyn et al., 2011, doi: 10.1111/j.1365-2486.2012.02790.x
+        """
+        gs = self.g0 + 1.6*(1 + self.g1/np.sqrt(D))*An/Cs
+        return gs
+
+    def Ficks_diffusion_Ci(self,Cs,An,gs):
+        Ci = Cs - 1.6*An/gs
+        return Ci
+        
+    def solve_Ci_conditional(self,Cs,Q,O,D,Vqmax,a1,phi1P_max,S,max_iterations=10,rtol_Ci=0.01):
+        """
+
+        Notes
+        -----
+        To determine Ci the photosynthetic rate is represented as the RuBP-regeneration limited (light-limited)
+        rate, following Medlyn et al. (2011, doi:10.1111/j.1365-2486.2010.02375.x and Corrigendum doi:10.1111/j.1365-2486.2012.02790.x)
+        and Medlyn et al. (2013, doi:10.1016/j.agrformet.2013.04.019).
+        """
+        #rtol_Ci: tolerance limit for Ci convergence expressed as a fraction of Cs
+        # Step 1 - Initial estimate of Ci
+        Ci =  0.7*Cs
+        tol = rtol_Ci*Cs
+        
+        for i in range(max_iterations):
+            A = self.JB21_A_j(Q,Ci,O,Vqmax,a1,phi1P_max,S)      # Step 2 - Calculate A with estimate of Ci
+            gs = self.gs_Medlyn(Cs,D,A)    # Step 3 - Update gs using the Medlyn formulation
+            Ci_new = self.Ficks_diffusion_Ci(Cs,A,gs)   # Step 4 - Update Ci using the formulation
+            if abs(Ci - Ci_new) < tol:   # Convergence criterion
+                break
+            Ci = Ci_new
+        return (A, gs, Ci)
+
+    def solve_Ci(self,Cs,Q,O,D,Vqmax,a1,phi1P_max,S,max_iterations=10,rtol_Ci=0.01):
+        _vfunc = np.vectorize(self.solve_Ci_conditional)
+        A, gs, Ci = _vfunc(Cs,Q,O,D,Vqmax,a1,phi1P_max,S)
+        return (A, gs, Ci)
+
+    def compute_A(self, C, O, Q, Vcmax, Vqmax, TPU, Gamma_star, Rd, S, Kc, Ko, a1, phi1P_max):
+        """NOTES HERE"""
+        
+        eta = (1-(self.nl/self.nc)+(3+7*Gamma_star/C)/((4+8*Gamma_star/C)*self.nc))  # Calculate PS I/II ETR
+
+        # This is equation 16.5 in Bonan (2015) Ecological Climatology
+        # Ve          = Je * CO2_per_electron
+        # JB21 Model
+        # Expressions for potential Cytochrome b6f-limited rates (_j)
+        #   N.B., see Eqns. 30-31
+        #ETR_eta     = (1-(self.nl/self.nc)+(3+7.*Gamma_star/Ci)/((4+8.*Gamma_star/Ci)*self.nc)); # PS I/II ETR, Eqn. 15c
+        #JP700_j = (Q*Vqmax)/(Q+Vqmax/(a1*phi1P_max))
+        #JP680_j = JP700_j/ETR_eta
+        #Ve      = JP680_j * CO2_per_electron    # Ve == Ag_j in JB21 model_fun.m
+        Ve  = self.JB21_A_j(Q, C, O, Vqmax, a1, phi1P_max, S)
+
+        # Expressions for potential Rubisco-limited rates (_c)
+        #   N.B., see Eqns. 32-33
+        Vc  = self.JB21_A_c(C, O, Vcmax, S, Kc, Ko)
+
+        JP680_c = Vc*4*(1+2*Gamma_star/C)/(1-Gamma_star/C)
+        JP700_c = JP680_c*eta
+
+        Vs  = 3*TPU
+
+        # Here we allow for co-limitation of Vc and Ve, providing a smooth transition between the 
+        # two potentially limiting rates. This takes the smaller root of a quadratic equation,  
+        # where 0.98 is the empirical smoothing parameter. See Bonan (2019) Eq. 11.31-11.33
+        V    = self.sel_root_conditional( self.atheta, -(Vc+Ve), Vc*Ve, np.sign(-Vc) )
+
+        Ag   = self.sel_root_conditional( self.atheta, -(V+Vs), V*Vs, -1 )
+
+        A    = Ag - Rd
+
+        # Select minimum PS1 ETR
+        #JP700_a = self.sel_root_conditional( self.atheta, -(JP700_j+JP700_c), JP700_j*JP700_c, np.sign(-Vc) )
+
+        # Select minimum PS2 ETR 
+        #JP680_a = self.sel_root_conditional( self.atheta, -(JP680_j+JP680_c), JP680_j*JP680_c, np.sign(-Vc) )
+
+        return (A, Ag, Vc, Ve, Vs) # , JP680_a, JP700_a)
+
+    def sel_root(self, a, b, c, dsign):
+        """
+        sel_root - select a root based on the fourth arg (dsign = discriminant sign) for the eqn ax^2 + bx + c,
+        if dsign is:
+            -1, 0: choose the smaller root
+            +1: choose the larger root
+        NOTE: technically, we should check a, but in biochemical, a is always > 0
+        """
+        if a == 0:  #% note: this works because 'a' is a scalar parameter!
+            x      = -c/b
+        else:
+            if dsign == 0:            
+                dsign = -1  #% technically, dsign==0 iff b = c = 0, so this isn't strictly necessary except, possibly for ill-formed cases)
+            
+            #%disc_root = sqrt(b.^2 - 4.*a.*c); % square root of the discriminant (doesn't need a separate line anymore)
+            #%  in MATLAB (2013b) assigning the intermediate variable actually slows down the code! (~25%)
+            x = (-b + dsign* np.sqrt(b**2 - 4*a*c))/(2*a)
+        
+        return x
+
+    def sel_root_conditional(self, a, b, c, dsign):
+        _vfunc = np.vectorize(self.sel_root)
+        x = _vfunc(a,b,c,dsign)
+        return x

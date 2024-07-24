0d5adb0ed24d83eba4af41acad0cc8880ae89c60
Alexander Norton <alexander.norton@climate-energy-college.org>
2024-07-24 16:38:58 +1000

    Update some canopylayer methods to handle array_like inputs
    This is working towards a model code structure that allows methods
    to be run with scalar inputs or array_like inputs (e.g. for running
    time-series or diagnostics on a set of outputs).

--- src/daesim/canopylayers.py
+++ src/daesim/canopylayers.py
@@ -1,6 +1,7 @@
 import numpy as np
 from attrs import define, field
 from scipy.stats import beta
+from typing import Union
 
 @define
 class CanopyLayers:
@@ -101,24 +102,38 @@ class CanopyLayers:
     #     self.nlevmlcan = self.nlayers(L,z)
 
 
-    def cast_parameter_over_layers_uniform(self,p):
+    def cast_parameter_over_layers_uniform(self,p: Union[float, np.ndarray]) -> np.ndarray:
         """
         Assigns vertically resolved parameter assuming constant value over discrete canopy layers.
 
         Parameters
         ----------
-        p : float
-            Parameter value
+        p : float or array_like
+            Parameter value or array of parameter values
 
         Returns
         -------
-        layer_p : array_like
-            Parameter value at each canopy height
+        layer_p : np.ndarray
+            Parameter value at each canopy height, or an array of parameter values at each canopy height for each input parameter
+
+        Notes
+        -----
+        - The function can take float or array_like inputs for `p` to provide flexibility in specifying
+          either single values or multiple values for the parameter.
+        - The function uses broadcasting to handle array_like inputs efficiently, allowing operations on 
+          multi-dimensional arrays without explicit loops.
         """
-        layer_p = p*np.ones(self.nlevmlcan)  ## assign the parameter value over the levels of the multilayer canopy
+        p = np.asarray(p)
+        # Assign the parameter value over the levels of the multilayer canopy
+        layer_p = np.broadcast_to(p[:, np.newaxis] if p.ndim == 1 else p, (p.size, self.nlevmlcan) if p.ndim == 1 else (self.nlevmlcan,))
         return layer_p
 
-    def cast_parameter_over_layers_exp(self,ptop,k,L_C):
+    def cast_parameter_over_layers_exp(
+        self,
+        ptop: Union[float, np.ndarray],
+        k: float,
+        L_C: Union[float, np.ndarray]
+        ) -> np.ndarray:
         """
         Calculate parameter at each canopy height using an exponential decay
         function. The vertical distribution of the parameter follows a 
@@ -127,11 +142,11 @@ class CanopyLayers:
         
         Parameters
         ----------
-        ptop : float
+        ptop : float or array_like
             Parameter value at top-of-canopy
         k : float
             Exponential function shape parameter (exponential decay rate)
-        L_C : float
+        L_C : float or array_like
             Canopy leaf area index (total LAI over whole canopy), m2 m-2
 
         Returns
@@ -149,25 +164,45 @@ class CanopyLayers:
         ----------
         De Pury and Farquhar, 1997, doi:10.1111/j.1365-3040.1997.00094.x
         """
+        # Ensure each input is a numpy array to allow broadcasting
+        ptop = np.asarray(ptop)
+        k = np.asarray(k)
+        L_C = np.asarray(L_C)
+
+        # Retrieve canopy indices
+        ntop, nbot = self.index_canopy()
+        
         dlai = self.cast_parameter_over_layers_betacdf(L_C,self.beta_lai_a,self.beta_lai_b)
         ## Determine cumulative LAI from top of the canopy
         ## First, determine which index of dlai array is the top of the canopy
         itop = self.index_canopy()[0]
-        if itop == 0:
-            L = np.cumsum(dlai)
-        else:
-            L = np.cumsum(dlai[::-1])
-
+        if itop != 0:
+            dlai = np.flip(dlai, axis=-1 if dlai.ndim == 2 else 0)
+        
+        # Compute cumulative sum
+        L = np.cumsum(dlai, axis=-1 if dlai.ndim == 2 else 0)
+        
+        # Ensure ptop and L_C are broadcasted correctly
+        ptop = np.broadcast_to(ptop[:, np.newaxis] if ptop.ndim == 1 else ptop, (ptop.size, 1) if ptop.ndim == 1 else (1,))
+        L_C_broadcasted = np.broadcast_to(L_C[:, np.newaxis] if L_C.ndim == 1 else L_C, (L_C.size, self.nlevmlcan) if L_C.ndim == 1 else (self.nlevmlcan,))
+        
         ## Cumulative relative LAI from top of the canopy
-        relative_LAI = L/L_C
+        relative_LAI = L/L_C_broadcasted
+        
         ## Determine the parameter value over the levels of the multilayer using an exponentially declining function
         layer_p = ptop * np.exp(-k*relative_LAI)
-
-        ## Return multilayer in same index order as rest of canopy
-        if itop == 0:
-            return layer_p
+        
+        # # Assign LAI to each layer
+        if layer_p.ndim == 1:
+            if ntop > nbot:
+                # Reverse order of array so that top-of-canopy is the last index
+                layer_p = layer_p[::-1]
         else:
-            return layer_p[::-1]
+            if ntop > nbot:
+                # Reverse order of array so that top-of-canopy is the last index
+                layer_p = np.flip(layer_p, axis=-1)
+
+        return layer_p
 
     def cast_scalefactor_to_layer_exp(self,k,L_C,cumulative_LAI):
         """
@@ -196,7 +231,12 @@ class CanopyLayers:
         layer_scalefactor = np.exp(-k * relative_LAI)
         return layer_scalefactor
 
-    def cast_parameter_over_layers_betacdf(self,p,alpha_param,beta_param,method="total"):
+    def cast_parameter_over_layers_betacdf(self,
+        p: Union[float, np.ndarray],
+        alpha_param: Union[float, np.ndarray],
+        beta_param: Union[float, np.ndarray],
+        method: str = "total"
+        ) -> np.ndarray:
         """
         Calculate parameter at each canopy height using the beta distribution. Use the cumulative
         distribution function evaluated at the the bottom and top heights for the layer. The shape 
@@ -205,11 +245,11 @@ class CanopyLayers:
 
         Parameters
         ----------
-        p : float
+        p : float or array_like
             Canopy level parameter
-        alpha_param : float
+        alpha_param : float or array_like
             Beta distribution parameter 'alpha'
-        beta_param : float
+        beta_param : float or array_like
             Beta distribution parameter 'beta'
         method : str
             "total" = the parameter p represents the canopy total, such that: canopy p = integral of multi-layer p generated from this function
@@ -217,36 +257,65 @@ class CanopyLayers:
 
         Returns
         -------
-        layer_p : array_like
-            Parameter value at each canopy height
+        layer_p : np.ndarray
+            Parameter value at each canopy height, or an array of parameter values at each canopy height for each input parameter
+
+        Notes
+        -----
+        - The function can take float or array_like inputs for `p`, `alpha_param`, and `beta_param` to provide flexibility in specifying
+          either single values or multiple values for these parameters.
+        - The function uses broadcasting to handle array_like inputs efficiently, allowing operations on multi-dimensional arrays without
+          explicit loops.
+        - Depending on the method selected, `p` is either used directly ("total") or multiplied by `Canopy.nlevmlcan` ("average") to determine
+          `pvar`, which represents the parameter to be distributed across the canopy layers.
+        - The layer boundaries are calculated using `np.linspace`, and the cumulative distribution function (CDF) of the beta distribution is
+          used to determine the fractions of LAI assigned to each layer.
+        - If `ntop > nbot`, the order of the layers is reversed to ensure that the top of the canopy is the last index in the output array.
 
         References
         ----------
         Bonan et al., 2021, doi:10.1016/j.agrformet.2021.108435
         """
+        # Ensure each input is a numpy array to allow broadcasting
+        alpha_param = np.asarray(alpha_param)
+        beta_param = np.asarray(beta_param)
+        p = np.asarray(p)
 
+        # Determine pvar based on the method
         if method == "total":
             pvar = p
         elif method == "average":
             pvar = p * self.nlevmlcan
         else:
-            print("Error: Chosen method, %s, for casting parameter over canopy layers not available" % method)
-        
+            raise ValueError(f"Error: Chosen method, {method}, for casting parameter over canopy layers not available")
+
         # Generate the boundaries of the layers in the canopy
         layer_bounds = np.linspace(0, 1, self.nlevmlcan+1)
-        
+
+        # Ensure alpha_param and beta_param are broadcasted correctly
+        alpha_param = np.broadcast_to(alpha_param[:, np.newaxis] if alpha_param.ndim == 1 else alpha_param, (alpha_param.size, self.nlevmlcan+1) if alpha_param.ndim == 1 else (self.nlevmlcan+1,))
+        beta_param = np.broadcast_to(beta_param[:, np.newaxis] if beta_param.ndim == 1 else beta_param, (beta_param.size, self.nlevmlcan+1) if beta_param.ndim == 1 else (self.nlevmlcan+1,))
+
         # Calculate the CDF values at these boundaries
         cdf_values = beta.cdf(layer_bounds, alpha_param, beta_param)
         
         # Calculate the fraction of variable for each layer by finding the difference between successive CDF values
-        fractions = np.diff(cdf_values)
+        fractions = np.diff(cdf_values, axis=-1)
         
-        # Assign LAI to each layer
-        layer_p = fractions * pvar
-
+        # Retrieve canopy indices
         ntop, nbot = self.index_canopy()
-        if ntop > nbot:
-            return layer_p[::-1]
+
+        # Assign LAI to each layer
+        if pvar.size == 1:
+            layer_p = fractions * pvar
+            if ntop > nbot:
+                # Reverse order of array so that top-of-canopy is the last index
+                layer_p = layer_p[::-1]
         else:
-            return layer_p
+            layer_p = (fractions * pvar[:, np.newaxis])
+            if ntop > nbot:
+                # Reverse order of array along canopy dimension so that top-of-canopy is the last index
+                layer_p = layer_p[:,::-1]
+
+        return layer_p
             
